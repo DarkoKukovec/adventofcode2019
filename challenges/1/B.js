522ef3dc17b631181b0e1da472531d88b181e506
Darko Kukovec <darko@infinum.hr>
2019-12-01 15:25:42 +0100

    There is no return

--- challenges/1/B.js
+++ challenges/1/B.js
@@ -11,13 +11,13 @@ const { parse, sum } = require('../../operators');
 
 module.exports = [
   parse('\n', true),
-  switchMap((data) => {
-    return of(data).pipe(
+  switchMap((data) =>
+    of(data).pipe(
       expand((val) => (val < 1 ? throwError() : of(Math.floor(val / 3) - 2))),
       catchError(() => of(0)),
       filter((x) => x > 0),
       skip(1),
-    );
-  }),
+    ),
+  ),
   sum(),
 ];
--- operators.js
+++ operators.js
@@ -1,14 +1,12 @@
 const { of } = require('rxjs');
-const { reduce, switchMap } = require('rxjs/operators');
+const { map, reduce, switchMap } = require('rxjs/operators');
 
 module.exports = {
-  parse(separator = '\n', parse) {
-    return switchMap((data) => {
-      const val = data.split(separator);
-      return parse ? of(...val.map((x) => parseInt(x, 10))) : of(...val);
-    });
-  },
-  sum() {
-    return reduce((agg, curr) => agg + curr);
-  },
+  parse: (separator = '\n', parse) =>
+    switchMap((data) =>
+      of(...data.split(separator)).pipe(
+        map((x) => (parse ? parseInt(x, 10) : x)),
+      ),
+    ),
+  sum: () => reduce((agg, curr) => agg + curr),
 };
