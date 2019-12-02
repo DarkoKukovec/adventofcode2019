35d2619c0a3579e978a9ac4669cbe9244ca9c319
Darko Kukovec <darko@infinum.hr>
2019-12-02 20:16:45 +0100

    Extract generic program

--- challenges/2/B.js
+++ challenges/2/B.js
@@ -1,15 +1,6 @@
 const { combineLatest, of, range } = require('rxjs');
-const {
-  expand,
-  map,
-  switchMap,
-  toArray,
-  filter,
-  find,
-  takeWhile,
-  takeLast,
-} = require('rxjs/operators');
-const { parse } = require('../../operators');
+const { map, switchMap, toArray, filter, find } = require('rxjs/operators');
+const { parse, programWithVerbs } = require('../../operators');
 
 module.exports = [
   parse(','),
@@ -29,30 +20,7 @@ module.exports = [
       switchMap((val) => of(...val)),
     ),
   ),
-  switchMap(([val, noun, verb]) =>
-    of([val, noun, verb]).pipe(
-      map(([val, noun, verb]) => [0, [val[0], noun, verb, ...val.slice(3)]]),
-      map(([position, val]) => [
-        position,
-        val,
-        val.slice(position, position + 4),
-      ]),
-      expand(([position, program, [cmd, a, b, x]]) => {
-        if (cmd === 99) {
-          return of([program[0], noun, verb]);
-        }
-        program[x] =
-          cmd === 1 ? program[a] + program[b] : program[a] * program[b];
-        return of([
-          position + 4,
-          program,
-          program.slice(position + 4, position + 8),
-        ]);
-      }),
-      takeWhile(([_position, program]) => program instanceof Array, true),
-      takeLast(1),
-    ),
-  ),
+  programWithVerbs(),
   filter((val) => val.length === 3, true),
   find(([val]) => val === 19690720),
   map(([_, noun, verb]) => 100 * noun + verb),
--- operators.js
+++ operators.js
@@ -1,5 +1,12 @@
 const { of } = require('rxjs');
-const { map, reduce, switchMap } = require('rxjs/operators');
+const {
+  expand,
+  map,
+  reduce,
+  switchMap,
+  takeLast,
+  takeWhile,
+} = require('rxjs/operators');
 
 module.exports = {
   parse: (separator = '\n', parseNumber = true) =>
@@ -9,4 +16,35 @@ module.exports = {
       ),
     ),
   sum: () => reduce((agg, curr) => agg + curr),
+  programWithVerbs: () =>
+    switchMap(([val, noun, verb]) =>
+      of([val, noun, verb]).pipe(
+        map(([val, noun, verb]) => [0, [val[0], noun, verb, ...val.slice(3)]]),
+        module.exports.program(noun, verb),
+      ),
+    ),
+  program: (noun, verb) =>
+    switchMap(([position, val]) =>
+      of([position, val]).pipe(
+        map(([position, val]) => [
+          position,
+          val,
+          val.slice(position, position + 4),
+        ]),
+        expand(([position, program, [cmd, a, b, x]]) => {
+          if (cmd === 99) {
+            return of([program[0], noun, verb]);
+          }
+          program[x] =
+            cmd === 1 ? program[a] + program[b] : program[a] * program[b];
+          return of([
+            position + 4,
+            program,
+            program.slice(position + 4, position + 8),
+          ]);
+        }),
+        takeWhile(([_position, program]) => program instanceof Array, true),
+        takeLast(1),
+      ),
+    ),
 };
